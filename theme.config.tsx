720b469348bcb3b4341bc29d403060d18e30752a
Jonathan Osacky <jonathan.osacky@gmail.com>
2023-10-21 16:19:07 -0400

    Commit and fix issues

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,9 +3,6 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Meter App</span>,
-  chat: {
-    link: 'https://wa.me/+573004975796',
-  },
 }
 
 export default config
