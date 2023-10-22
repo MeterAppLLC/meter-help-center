f4f0377211821732211ceaf3b2d8722066e3d9f9
Jonathan Osacky <jonathan.osacky@gmail.com>
2023-10-22 14:02:26 -0400

    Add translations

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import {DocsThemeConfig} from 'nextra-theme-docs'
+import useLocalesMap from "./components/use-locales-map";
+import {searchPlaceholderMap, tableOfContentsTitleMap} from "./components/text";
 const config: DocsThemeConfig = {
     logo: <span>Meter App</span>,
     docsRepositoryBase: 'https://github.com/MeterAppLLC/meter-help-center',
     feedback: {
         content: null
+    },
+    search: {
+        placeholder: () => useLocalesMap(searchPlaceholderMap),
+    },
+    toc: {
+        float: true,
+        title: () => useLocalesMap(tableOfContentsTitleMap),
     },
     editLink: {
         text: null
