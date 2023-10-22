629b2d9d938da4e3c36c06dcdf43fab67c948e56
Jonathan Osacky <jonathan.osacky@gmail.com>
2023-10-22 13:57:11 -0400

    Try to fix build

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,8 @@
 import React from 'react'
 import {DocsThemeConfig} from 'nextra-theme-docs'
-import useLocalesMap from "./pages/component/use-locales-map";
-import {useRouter} from "next/router";
-import {gitTimestampMap, searchPlaceholderMap, tableOfContentsTitleMap} from "./pages/translations/text";
 const config: DocsThemeConfig = {
     logo: <span>Meter App</span>,
     docsRepositoryBase: 'https://github.com/MeterAppLLC/meter-help-center',
-    search: {
-        placeholder: () => useLocalesMap(searchPlaceholderMap),
-    },
-    toc: {
-        float: true,
-        title: () => useLocalesMap(tableOfContentsTitleMap),
-    },
     feedback: {
         content: null
     },
@@ -21,23 +11,6 @@ const config: DocsThemeConfig = {
     },
     footer: {
         text: 'Meter App LLC',
-    },
-    gitTimestamp({timestamp}) {
-        const {locale} = useRouter();
-        const lastUpdatedOn = useLocalesMap(gitTimestampMap);
-
-        return (
-            <>
-                {lastUpdatedOn}{" "}
-                <time dateTime={timestamp.toISOString()}>
-                    {timestamp.toLocaleDateString(locale, {
-                        day: "numeric",
-                        month: "long",
-                        year: "numeric",
-                    })}
-                </time>
-            </>
-        );
     },
     i18n: [
         {locale: 'en-US', text: 'English'},
