0d1ea3c24d865a8d0c44946069e784d55cad8940
Jonathan Osacky <jonathan.osacky@gmail.com>
2023-10-22 11:18:03 -0400

    Fix middleware

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,13 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Meter App</span>,
+  docsRepositoryBase: 'https://github.com/MeterAppLLC/meter-help-center',
+  feedback : {
+    content: null
+  },
+  editLink: {
+    text: ''
+  }
 }
 
 export default config
