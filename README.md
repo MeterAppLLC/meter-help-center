b34fe361a6f73e0ec8d4bdfd6a75ac8be10ebe82
Jonathan Osacky <jonathan.osacky@gmail.com>
2023-10-22 17:25:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Meter Help Center
+
+Feel free to edit the pages inside the /pages directory and submit a request to change for https://help.meterapp.co/
---
+++ theme.config.js
@@ -0,0 +1,63 @@
+import {useRouter} from "next/router";
+import useLocalesMap from "./components/use-locales-map";
+import {
+    editTextMap,
+    feedbackLinkMap,
+    gitTimestampMap,
+    languageMap,
+    searchPlaceholderMap,
+    tableOfContentsTitleMap,
+} from "./translations/text";
+
+/** @type {import('nextra-theme-docs').DocsThemeConfig} */
+const themeConfig = {
+    project: {
+        link: "https://github.com/MeterAppLLC/meter-help-center",
+    },
+    docsRepositoryBase: "https://github.com/MeterAppLLC/meter-help-center/blob/main",
+    useNextSeoProps() {
+        return {
+            titleTemplate: "%s – Meter App Help Center",
+        };
+    },
+    toc: {
+        float: true,
+        title: () => useLocalesMap(tableOfContentsTitleMap),
+    },
+    search: {
+        placeholder: () => useLocalesMap(searchPlaceholderMap),
+    },
+    editLink: {
+        text: () => useLocalesMap(editTextMap),
+    },
+    feedback: {
+        content: () => useLocalesMap(feedbackLinkMap),
+    },
+    logo: <span>Meter App</span>,
+    footer: {
+        text: 'Meter App LLC',
+    },
+    gitTimestamp({timestamp}) {
+        const {locale} = useRouter();
+        const lastUpdatedOn = useLocalesMap(gitTimestampMap);
+
+        return (
+            <>
+                {lastUpdatedOn}{" "}
+                <time dateTime={timestamp.toISOString()}>
+                    {timestamp.toLocaleDateString(locale, {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                    })}
+                </time>
+            </>
+        );
+    },
+    i18n: Object.entries(languageMap).map(([locale, text]) => ({
+        locale,
+        text,
+    })),
+};
+
+export default themeConfig;
